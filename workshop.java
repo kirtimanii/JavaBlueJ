29c597f6012a47b2767add65e3d8eb49b184e8df
Kirtimani Tamang <kirtimanitamang@gmail.com>
2025-11-19 10:01:37 +0545

    changed print statement

--- workshop.java
+++ workshop.java
@@ -21,6 +21,7 @@ public class workshop
         
         System.out.println(age);
         System.out.println(workshop.qty);
+        System.out.println("third week of programming");
         
         
     }
